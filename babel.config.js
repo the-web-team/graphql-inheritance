fbf63079c2be5f42690925aa3d291486bd4af9c1
Vinlock <dak@pvpallday.com>
2019-07-19 21:31:36 -0700

    Setup meta/dev files

---
+++ babel.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  plugins: [
+    'transform-class-properties',
+    'transform-object-rest-spread',
+    'transform-es2015-modules-commonjs',
+  ],
+  'presets': [
+    'env',
+  ],
+};
